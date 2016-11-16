e178256512ba8f7d8d8281e8310a1338667dc4d2
André <a_souzalandy@yahoo.com.br>
2016-11-16 17:40:09 -0200

    Atividade 56 - Metodos genericos de aggregated relationship

---
+++ br.ufscar.ARCH-KDM.core/src/br/ufscar/ARCH_KDM/util/GenericMethods.java
@@ -0,0 +1,40 @@
+/**
+ * @author Landi
+ * 
+ */
+package br.ufscar.ARCH_KDM.util;
+
+import java.util.List;
+
+import org.eclipse.gmt.modisco.omg.kdm.core.AggregatedRelationship;
+import org.eclipse.gmt.modisco.omg.kdm.core.CoreFactory;
+import org.eclipse.gmt.modisco.omg.kdm.core.KDMRelationship;
+import org.eclipse.gmt.modisco.omg.kdm.structure.AbstractStructureElement;
+
+/**
+ * @author Landi
+ *
+ */
+public class GenericMethods {
+
+	public static void createAggreagatedWith(AbstractStructureElement to, AbstractStructureElement from,
+			List<KDMRelationship> relations) {
+		
+		AggregatedRelationship aggregatedRelationship = CoreFactory.eINSTANCE.createAggregatedRelationship();
+		aggregatedRelationship.setDensity(relations.size());
+		aggregatedRelationship.setFrom(from);
+		aggregatedRelationship.setTo(to);
+		aggregatedRelationship.getRelation().addAll(relations);
+		
+		from.getAggregated().add(aggregatedRelationship);
+	}
+	
+	public static void updateAggreagatedWith(AggregatedRelationship aggregatedRelationship, List<KDMRelationship> relations) {
+
+		int density = relations.size() + aggregatedRelationship.getDensity();
+		aggregatedRelationship.setDensity(density);
+		aggregatedRelationship.getRelation().addAll(relations);
+
+	}
+	
+}
