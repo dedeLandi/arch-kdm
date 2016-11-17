3f04d645116c12c21fa692973f4eacf8086c44b7
André <a_souzalandy@yahoo.com.br>
2016-11-17 19:13:25 -0200

    Atividade 56 - Metodos genericos
    Metodos para leitura de modelos e arquivos

--- br.ufscar.ARCH-KDM.core/src/br/ufscar/arch_kdm/core/util/GenericMethods.java
+++ br.ufscar.ARCH-KDM.core/src/br/ufscar/arch_kdm/core/util/GenericMethods.java
@@ -5,11 +5,20 @@
 package br.ufscar.arch_kdm.core.util;
 
 import java.util.List;
+import java.util.Map;
 
+import org.eclipse.gmt.modisco.omg.kdm.code.CodeModel;
 import org.eclipse.gmt.modisco.omg.kdm.core.AggregatedRelationship;
 import org.eclipse.gmt.modisco.omg.kdm.core.CoreFactory;
 import org.eclipse.gmt.modisco.omg.kdm.core.KDMRelationship;
+import org.eclipse.gmt.modisco.omg.kdm.kdm.Segment;
 import org.eclipse.gmt.modisco.omg.kdm.structure.AbstractStructureElement;
+import org.eclipse.gmt.modisco.omg.kdm.structure.StructureModel;
+
+import br.ufscar.kdm_manager.core.exceptions.KDMFileException;
+import br.ufscar.kdm_manager.core.loads.factory.KDMFileReaderFactory;
+import br.ufscar.kdm_manager.core.readers.modelReader.factory.KDMModelReaderJavaFactory;
+import br.ufscar.kdm_manager.core.serializes.factory.KDMFileSerializeFactory;
 
 /**
  * @author Landi
@@ -35,6 +44,46 @@ public class GenericMethods {
 		aggregatedRelationship.setDensity(density);
 		aggregatedRelationship.getRelation().addAll(relations);
 
+	}
+
+	/**
+	 * @author Landi
+	 */
+	public static void serializeSegment(String kdmPath, Segment segmentToSerialize) {
+
+		KDMFileSerializeFactory.eINSTANCE.createKDMFileSerializeFromSegment().serializeFromObject(kdmPath, segmentToSerialize);
+	}
+
+	/**
+	 * @author Landi
+	 * @param segment
+	 * @return
+	 */
+	public static Map<String, List<CodeModel>> getAllCode(Segment segment) {
+		return KDMModelReaderJavaFactory.eINSTANCE.createKDMCodeModelReader().getAllFromSegment(segment);
+	}
+
+	/**
+	 * @author Landi
+	 * @param segment
+	 * @return
+	 */
+	public static Map<String, List<StructureModel>> getAllStructure(Segment segment) {
+		return KDMModelReaderJavaFactory.eINSTANCE.createKDMStructureModelReader().getAllFromSegment(segment);
+	}
+
+	/**
+	 * @author Landi
+	 * @param path
+	 * @return
+	 */
+	public static Segment readSegmentFromPath(String path) {
+		try {
+			return KDMFileReaderFactory.eINSTANCE.createKDMFileReaderToSegment().readFromPath(path);
+		} catch (KDMFileException e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 	
 }
