97324d019a3b29882da718f571c8bf67fc8e5a0a
André <a_souzalandy@yahoo.com.br>
2016-11-19 15:17:57 -0200

    Atividade 56 - Metodos genericos
    Foi atualizado alguns metodos para uso de novas opções do KDM-MANAGER

--- br.ufscar.ARCH-KDM.core/src/br/ufscar/arch_kdm/core/util/GenericMethods.java
+++ br.ufscar.ARCH-KDM.core/src/br/ufscar/arch_kdm/core/util/GenericMethods.java
@@ -4,20 +4,33 @@
  */
 package br.ufscar.arch_kdm.core.util;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
+import org.eclipse.gmt.modisco.omg.kdm.code.AbstractCodeElement;
+import org.eclipse.gmt.modisco.omg.kdm.code.ClassUnit;
+import org.eclipse.gmt.modisco.omg.kdm.code.CodeItem;
 import org.eclipse.gmt.modisco.omg.kdm.code.CodeModel;
+import org.eclipse.gmt.modisco.omg.kdm.code.EnumeratedType;
+import org.eclipse.gmt.modisco.omg.kdm.code.InterfaceUnit;
+import org.eclipse.gmt.modisco.omg.kdm.code.Package;
 import org.eclipse.gmt.modisco.omg.kdm.core.AggregatedRelationship;
 import org.eclipse.gmt.modisco.omg.kdm.core.CoreFactory;
+import org.eclipse.gmt.modisco.omg.kdm.core.KDMEntity;
 import org.eclipse.gmt.modisco.omg.kdm.core.KDMRelationship;
 import org.eclipse.gmt.modisco.omg.kdm.kdm.Segment;
 import org.eclipse.gmt.modisco.omg.kdm.structure.AbstractStructureElement;
 import org.eclipse.gmt.modisco.omg.kdm.structure.StructureModel;
 
 import br.ufscar.kdm_manager.core.exceptions.KDMFileException;
+import br.ufscar.kdm_manager.core.filters.validateFilter.factory.KDMValidateFilterJavaFactory;
+import br.ufscar.kdm_manager.core.filters.validateFilter.interfaces.KDMValidateFilter;
 import br.ufscar.kdm_manager.core.loads.factory.KDMFileReaderFactory;
 import br.ufscar.kdm_manager.core.readers.modelReader.factory.KDMModelReaderJavaFactory;
+import br.ufscar.kdm_manager.core.recovers.recoverCodeHierarchy.factory.KDMRecoverCodeHierarchyJavaFactory;
+import br.ufscar.kdm_manager.core.recovers.recoverStructureHierarchy.factory.KDMRecoverStructureHierarchyJavaFactory;
 import br.ufscar.kdm_manager.core.serializes.factory.KDMFileSerializeFactory;
 
 /**
@@ -28,16 +41,16 @@ public class GenericMethods {
 
 	public static void createAggreagatedWith(AbstractStructureElement to, AbstractStructureElement from,
 			List<KDMRelationship> relations) {
-		
+
 		AggregatedRelationship aggregatedRelationship = CoreFactory.eINSTANCE.createAggregatedRelationship();
 		aggregatedRelationship.setDensity(relations.size());
 		aggregatedRelationship.setFrom(from);
 		aggregatedRelationship.setTo(to);
 		aggregatedRelationship.getRelation().addAll(relations);
-		
+
 		from.getAggregated().add(aggregatedRelationship);
 	}
-	
+
 	public static void updateAggreagatedWith(AggregatedRelationship aggregatedRelationship, List<KDMRelationship> relations) {
 
 		int density = relations.size() + aggregatedRelationship.getDensity();
@@ -50,7 +63,6 @@ public class GenericMethods {
 	 * @author Landi
 	 */
 	public static void serializeSegment(String kdmPath, Segment segmentToSerialize) {
-
 		KDMFileSerializeFactory.eINSTANCE.createKDMFileSerializeFromSegment().serializeFromObject(kdmPath, segmentToSerialize);
 	}
 
@@ -85,5 +97,207 @@ public class GenericMethods {
 			return null;
 		}
 	}
+
+	/**
+	 * @author Landi
+	 * @param segment 
+	 * @param string
+	 * @return
+	 */
+	public static StructureModel getStructureArchitecture(String name, Segment segment) {
+		KDMValidateFilter<?, ?> filter = KDMValidateFilterJavaFactory.eINSTANCE.createValidateFilterNameOfKDMFramework(name);
+		Map<String, List<StructureModel>> allStructureModelActualArchitecture = KDMModelReaderJavaFactory.eINSTANCE.createKDMStructureModelReaderWithFilter(filter).getAllFromSegment(segment);
+		if(allStructureModelActualArchitecture.keySet().size() == 1){
+			for (String key : allStructureModelActualArchitecture.keySet()) {
+				if(allStructureModelActualArchitecture.get(key).size() == 1){
+					return allStructureModelActualArchitecture.get(key).get(0);
+				}
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * @author Landi
+	 * @param kdmEntity
+	 * @param originalCode
+	 * @return
+	 */
+	public static KDMEntity getOriginalCodeElement(KDMEntity elementToSearch, CodeModel originalCode) {
+		KDMEntity original = null;
+		for (AbstractCodeElement originalElement : originalCode.getCodeElement()) {
+			original = getOriginalCodeElement(elementToSearch, originalElement);
+			if(original != null){
+				break;
+			}
+		}
+		return original;
+	}
+
+	/**
+	 * @author Landi
+	 * @param elementToSearch
+	 * @param originalElement
+	 * @return 
+	 */
+	private static KDMEntity getOriginalCodeElement(KDMEntity elementToSearch, AbstractCodeElement originalElement) {
+		String pathElementToSearch = getPathFromCodeElement(elementToSearch);
+		String pathOriginalElement = getPathFromCodeElement(originalElement);
+		if(pathOriginalElement == null){
+			return null;
+		}
+		if(pathElementToSearch.equalsIgnoreCase(pathOriginalElement)){
+			return originalElement;			
+		}
+		KDMEntity original = null;
+		if(originalElement instanceof Package){
+			for (AbstractCodeElement child : ((Package) originalElement).getCodeElement()) {
+				original = getOriginalCodeElement(elementToSearch, child);
+				if (original != null) {
+					break;
+				}
+			}
+		}else if (originalElement instanceof ClassUnit) {
+			for (CodeItem child : ((ClassUnit) originalElement).getCodeElement()) {
+				original = getOriginalCodeElement(elementToSearch, child);
+				if (original != null) {
+					break;
+				}
+			}
+		}else if (originalElement instanceof InterfaceUnit) {
+			for (CodeItem child : ((InterfaceUnit) originalElement).getCodeElement()) {
+				original = getOriginalCodeElement(elementToSearch, child);
+				if (original != null) {
+					break;
+				}
+			}
+		}else if (originalElement instanceof EnumeratedType) {
+			for (CodeItem child : ((EnumeratedType) originalElement).getCodeElement()) {
+				original = getOriginalCodeElement(elementToSearch, child);
+				if (original != null) {
+					break;
+				}
+			}
+		}
+		return original;
+	}
+
+	/**
+	 * @author Landi
+	 * @param element
+	 * @param originalStructure
+	 * @return
+	 */
+	public static Collection<KDMEntity> getImplementationOf(AbstractStructureElement elementToSearch, StructureModel originalStructure) {
+		Collection<KDMEntity> list = new ArrayList<>();
+
+		for (AbstractStructureElement originalElement : originalStructure.getStructureElement()) {
+			list.addAll(getImplementationOf(elementToSearch, originalElement));
+			if(list.size() > 0){
+				return list;
+			}
+		}
+		return list;
+	}
+
+	/**
+	 * @author Landi
+	 * @param elementToSearch
+	 * @param originalElement
+	 * @return
+	 */
+	private static Collection<KDMEntity> getImplementationOf(AbstractStructureElement elementToSearch,
+			AbstractStructureElement originalElement) {
+		if(elementToSearch.getClass() == originalElement.getClass() && elementToSearch.getName().equalsIgnoreCase(originalElement.getName())){
+			return originalElement.getImplementation();			
+		}
+		Collection<KDMEntity> list = new ArrayList<>();
+		for (AbstractStructureElement child : originalElement.getStructureElement()) {
+			list.addAll(getImplementationOf(elementToSearch, child));
+		}
+		return list;
+	}
+
 	
+	/**
+	 * @author Landi
+	 * @param elementToSearch
+	 * @return
+	 */
+	public static String getPathFromCodeElement(KDMEntity elementToSearch) {
+		String path = null;
+
+		if(elementToSearch instanceof Package){
+			path = KDMRecoverCodeHierarchyJavaFactory.eINSTANCE.createRecoverCodeHierarchyComplete().getHierarchyOf((Package)elementToSearch);
+		}else if (elementToSearch instanceof ClassUnit) {
+			path = KDMRecoverCodeHierarchyJavaFactory.eINSTANCE.createRecoverCodeHierarchyComplete().getHierarchyOf((ClassUnit)elementToSearch);
+		}else if (elementToSearch instanceof InterfaceUnit) {
+			path = KDMRecoverCodeHierarchyJavaFactory.eINSTANCE.createRecoverCodeHierarchyComplete().getHierarchyOf((InterfaceUnit)elementToSearch);
+		}else if (elementToSearch instanceof EnumeratedType) {
+			path = KDMRecoverCodeHierarchyJavaFactory.eINSTANCE.createRecoverCodeHierarchyComplete().getHierarchyOf((EnumeratedType)elementToSearch);
+		}
+		
+		return path;
+	}
+
+	/**
+	 * @author Landi
+	 * @param from
+	 * @param structureDrifts
+	 * @return
+	 */
+	public static AbstractStructureElement getOriginalStructureElement(KDMEntity elementToSearch, StructureModel originalStructure) {
+		AbstractStructureElement original = null;
+		for (AbstractStructureElement originalElement : originalStructure.getStructureElement()) {
+			original = getOriginalStructureElement((AbstractStructureElement)elementToSearch, originalElement);
+			if(original != null){
+				break;
+			}
+		}
+		return original;
+	}
+
+	/**
+	 * @author Landi
+	 * @param elementToSearch
+	 * @param originalElement
+	 * @return
+	 */
+	private static AbstractStructureElement getOriginalStructureElement(AbstractStructureElement elementToSearch, AbstractStructureElement originalElement) {
+		String pathElementToSearch = getPathFromStructureElement(elementToSearch);
+		String pathOriginalElement = getPathFromStructureElement(originalElement);
+		System.out.println(pathElementToSearch);
+		System.out.println(pathOriginalElement);
+		System.out.println();
+		if(pathOriginalElement == null){
+			return null;
+		}
+		if(pathElementToSearch.equalsIgnoreCase(pathOriginalElement)){
+			return originalElement;			
+		}
+		AbstractStructureElement original = null;
+		
+		for (AbstractStructureElement child : originalElement.getStructureElement()) {
+			original = getOriginalStructureElement(elementToSearch, child);
+			if (original != null) {
+				break;
+			}
+		}
+		
+		return original;
+	}
+
+	/**
+	 * @author Landi
+	 * @param elementToSearch
+	 * @return
+	 */
+	public static String getPathFromStructureElement(AbstractStructureElement elementToSearch) {
+		String path = null;
+
+			path = KDMRecoverStructureHierarchyJavaFactory.eINSTANCE.createRecoverStructureHierarchyModel().getHierarchyOf(elementToSearch);
+		
+		return path;
+	}
+
 }
