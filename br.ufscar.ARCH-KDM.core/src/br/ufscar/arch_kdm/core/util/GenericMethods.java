5820324ba78c3ffbe056914d2c4deac87f2f0723
André <a_souzalandy@yahoo.com.br>
2016-11-21 14:12:40 -0200

    Atividade 56 - Metodos genericos
    Modificação do metodo de geração de aggregated relationship e criação de
    metodos auxiliares da ACC

--- br.ufscar.ARCH-KDM.core/src/br/ufscar/arch_kdm/core/util/GenericMethods.java
+++ br.ufscar.ARCH-KDM.core/src/br/ufscar/arch_kdm/core/util/GenericMethods.java
@@ -6,6 +6,7 @@ package br.ufscar.arch_kdm.core.util;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -17,7 +18,6 @@ import org.eclipse.gmt.modisco.omg.kdm.code.EnumeratedType;
 import org.eclipse.gmt.modisco.omg.kdm.code.InterfaceUnit;
 import org.eclipse.gmt.modisco.omg.kdm.code.Package;
 import org.eclipse.gmt.modisco.omg.kdm.core.AggregatedRelationship;
-import org.eclipse.gmt.modisco.omg.kdm.core.CoreFactory;
 import org.eclipse.gmt.modisco.omg.kdm.core.KDMEntity;
 import org.eclipse.gmt.modisco.omg.kdm.core.KDMRelationship;
 import org.eclipse.gmt.modisco.omg.kdm.kdm.Segment;
@@ -29,6 +29,7 @@ import br.ufscar.kdm_manager.core.filters.validateFilter.factory.KDMValidateFilt
 import br.ufscar.kdm_manager.core.filters.validateFilter.interfaces.KDMValidateFilter;
 import br.ufscar.kdm_manager.core.loads.factory.KDMFileReaderFactory;
 import br.ufscar.kdm_manager.core.readers.modelReader.factory.KDMModelReaderJavaFactory;
+import br.ufscar.kdm_manager.core.readers.structureReader.factory.KDMStructureReaderJavaFactory;
 import br.ufscar.kdm_manager.core.recovers.recoverCodeHierarchy.factory.KDMRecoverCodeHierarchyJavaFactory;
 import br.ufscar.kdm_manager.core.recovers.recoverStructureHierarchy.factory.KDMRecoverStructureHierarchyJavaFactory;
 import br.ufscar.kdm_manager.core.serializes.factory.KDMFileSerializeFactory;
@@ -42,13 +43,14 @@ public class GenericMethods {
 	public static void createAggreagatedWith(AbstractStructureElement to, AbstractStructureElement from,
 			List<KDMRelationship> relations) {
 
-		AggregatedRelationship aggregatedRelationship = CoreFactory.eINSTANCE.createAggregatedRelationship();
+		AggregatedRelationship aggregatedRelationship = from.createAggregation(to);;
 		aggregatedRelationship.setDensity(relations.size());
-		aggregatedRelationship.setFrom(from);
-		aggregatedRelationship.setTo(to);
+//		aggregatedRelationship.setFrom(from);
+//		aggregatedRelationship.setTo(to);
 		aggregatedRelationship.getRelation().addAll(relations);
 
 		from.getAggregated().add(aggregatedRelationship);
+		
 	}
 
 	public static void updateAggreagatedWith(AggregatedRelationship aggregatedRelationship, List<KDMRelationship> relations) {
@@ -300,4 +302,140 @@ public class GenericMethods {
 		return path;
 	}
 
+	/**
+	 * @author Landi
+	 * @param plannedArchitecture
+	 * @return 
+	 * @return
+	 */
+	public static List<AggregatedRelationship> getAllAggregateds(StructureModel model) {
+		return KDMStructureReaderJavaFactory.eINSTANCE.createKDMAggregatedRelationshipReader().getAllFrom(model);
+	}
+
+	/**
+	 * @author Landi
+	 * @param from
+	 * @param to
+	 * @param aggregatedsActualArchitecture
+	 * @return
+	 */
+	public static List<AggregatedRelationship> getSpecificAggregated(KDMEntity from, KDMEntity to,
+			List<AggregatedRelationship> aggregatedsActualArchitecture) {
+		
+		List<AggregatedRelationship> aggregatedsFound = new ArrayList<AggregatedRelationship>();
+		
+		for (AggregatedRelationship aggregatedRelationship : aggregatedsActualArchitecture) {
+			
+			if(getPathFromStructureElement((AbstractStructureElement) aggregatedRelationship.getFrom()).
+					equalsIgnoreCase(getPathFromStructureElement((AbstractStructureElement) from)) 
+					
+					&& getPathFromStructureElement((AbstractStructureElement) aggregatedRelationship.getTo()).
+					equalsIgnoreCase(getPathFromStructureElement((AbstractStructureElement) to))){
+				aggregatedsFound.add(aggregatedRelationship);
+			}
+			
+		}
+		
+		return aggregatedsFound;
+	}
+
+	
+	/**
+	 * @author Landi
+	 * @param structureModel 
+	 * @param i
+	 */
+	public static void removeAggregatedRelationshipWithDensityEquals(StructureModel structureModel, int density) {
+
+		for (AbstractStructureElement abstractStructureElement : structureModel.getStructureElement()) {
+
+			removeAggregatedRelationshipWithDensityEquals(abstractStructureElement, density);
+			
+		}
+	}
+
+	/**
+	 * @author Landi
+	 * @param abstractStructureElement
+	 * @param density
+	 */
+	private static void removeAggregatedRelationshipWithDensityEquals(AbstractStructureElement abstractStructureElement,
+			int density) {
+		
+		for (Iterator<AggregatedRelationship> iterator = abstractStructureElement.getInAggregated().iterator(); iterator.hasNext();) {
+			AggregatedRelationship aggregatedRelationship = (AggregatedRelationship) iterator.next();
+			if(aggregatedRelationship.getDensity() == density){
+				iterator.remove();
+			}
+		}
+		for (Iterator<AggregatedRelationship> iterator = abstractStructureElement.getOutAggregated().iterator(); iterator.hasNext();) {
+			AggregatedRelationship aggregatedRelationship = (AggregatedRelationship) iterator.next();
+			if(aggregatedRelationship.getDensity() == density){
+				iterator.remove();
+			}
+		}
+		for (Iterator<AggregatedRelationship> iterator = abstractStructureElement.getAggregated().iterator(); iterator.hasNext();) {
+			AggregatedRelationship aggregatedRelationship = (AggregatedRelationship) iterator.next();
+			if(aggregatedRelationship.getDensity() == density){
+				iterator.remove();
+			}
+		}
+		
+		if(abstractStructureElement.getStructureElement().size() > 0){
+			for (AbstractStructureElement abstractStructureElementChild : abstractStructureElement.getStructureElement()) {
+				removeAggregatedRelationshipWithDensityEquals(abstractStructureElementChild, density);
+			}
+		}
+	}
+
+	/**
+	 * @author Landi
+	 * @param violations
+	 */
+	public static void removeAggregatedRelationshipToFromEquals(StructureModel structureModel) {
+		for (AbstractStructureElement abstractStructureElement : structureModel.getStructureElement()) {
+
+			removeAggregatedRelationshipToFromEquals(abstractStructureElement);
+			
+		}
+	}
+	
+	/**
+	 * @author Landi
+	 * @param abstractStructureElement
+	 * @param density
+	 */
+	private static void removeAggregatedRelationshipToFromEquals(AbstractStructureElement abstractStructureElement) {
+		
+		for (Iterator<AggregatedRelationship> iterator = abstractStructureElement.getInAggregated().iterator(); iterator.hasNext();) {
+			AggregatedRelationship aggregatedRelationship = (AggregatedRelationship) iterator.next();
+			if(aggregatedRelationship.getFrom() == null || aggregatedRelationship.getTo() == null || 
+					getPathFromStructureElement((AbstractStructureElement) aggregatedRelationship.getFrom()).
+					equalsIgnoreCase(getPathFromStructureElement((AbstractStructureElement) aggregatedRelationship.getTo()))){
+				iterator.remove();
+			}
+		}
+		for (Iterator<AggregatedRelationship> iterator = abstractStructureElement.getOutAggregated().iterator(); iterator.hasNext();) {
+			AggregatedRelationship aggregatedRelationship = (AggregatedRelationship) iterator.next();
+			if(aggregatedRelationship.getFrom() == null || aggregatedRelationship.getTo() == null ||
+					getPathFromStructureElement((AbstractStructureElement) aggregatedRelationship.getFrom()).
+					equalsIgnoreCase(getPathFromStructureElement((AbstractStructureElement) aggregatedRelationship.getTo()))){
+				iterator.remove();
+			}
+		}
+		for (Iterator<AggregatedRelationship> iterator = abstractStructureElement.getAggregated().iterator(); iterator.hasNext();) {
+			AggregatedRelationship aggregatedRelationship = (AggregatedRelationship) iterator.next();
+			if(aggregatedRelationship.getFrom() == null || aggregatedRelationship.getTo() == null ||
+					getPathFromStructureElement((AbstractStructureElement) aggregatedRelationship.getFrom()).
+					equalsIgnoreCase(getPathFromStructureElement((AbstractStructureElement) aggregatedRelationship.getTo()))){
+				iterator.remove();
+			}
+		}
+		if(abstractStructureElement.getStructureElement().size() > 0){
+			for (AbstractStructureElement abstractStructureElementChild : abstractStructureElement.getStructureElement()) {
+				removeAggregatedRelationshipToFromEquals(abstractStructureElementChild);
+			}
+		}
+	}
+	
 }
