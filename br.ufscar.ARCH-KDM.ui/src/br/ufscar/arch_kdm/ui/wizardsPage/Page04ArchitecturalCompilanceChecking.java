2000c7e4c1d10fc81a5f4ac3145e36c327a1073e
André <a_souzalandy@yahoo.com.br>
2016-11-21 18:01:36 -0200

    Atividade 58 - Pagina 4 do wizard - Checagem de conformidade arquitetural
    Validação se a checagem foi chamada a partir da pagina dois ou pagina
    três

--- br.ufscar.ARCH-KDM.ui/src/br/ufscar/arch_kdm/ui/wizardsPage/Page04ArchitecturalCompilanceChecking.java
+++ br.ufscar.ARCH-KDM.ui/src/br/ufscar/arch_kdm/ui/wizardsPage/Page04ArchitecturalCompilanceChecking.java
@@ -5,8 +5,10 @@
 package br.ufscar.arch_kdm.ui.wizardsPage;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
 
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.gmt.modisco.omg.kdm.code.CodeModel;
 import org.eclipse.gmt.modisco.omg.kdm.kdm.Segment;
 import org.eclipse.gmt.modisco.omg.kdm.structure.StructureModel;
 import org.eclipse.jface.operation.IRunnableWithProgress;
@@ -19,13 +21,13 @@ import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
 
 import br.ufscar.arch_kdm.core.architecturalCompilanceChecking.ArchitecturalCompilanceChecking;
 import br.ufscar.arch_kdm.core.util.GenericMethods;
+import br.ufscar.arch_kdm.ui.util.InterfaceGenericMethods;
 import br.ufscar.arch_kdm.ui.wizards.ArchKDMWizard;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
 
 /**
  * @author Landi
@@ -34,11 +36,17 @@ import org.eclipse.swt.events.SelectionEvent;
 public class Page04ArchitecturalCompilanceChecking extends WizardPage {
 
 	private boolean canFlip = false;
-	private boolean analisisComplete = false;
+	private boolean accComplete = false;
 	protected Segment actualArchitecture;
 	protected Segment plannedArchitecture;
 	private ArchKDMWizard archKDMWizard;
-	
+	private String previousPage = "";
+	private Button btnInitiateProcessing;
+	protected StringBuilder accLog;
+	private String nameActualStructure;
+	private String namePlannedStructure;
+	private boolean canExecuteACC = true;
+
 	/**
 	 * Create the wizard.
 	 */
@@ -57,31 +65,36 @@ public class Page04ArchitecturalCompilanceChecking extends WizardPage {
 
 		setControl(container);
 		container.setLayout(new GridLayout(1, false));
-		
+
 		Label label = new Label(container, SWT.NONE);
 		label.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, true, false, 1, 1));
 		label.setText("For process this algorithm, possibly take a long time.\r\nIf you want to continue, click in the button below.");
 		label.setAlignment(SWT.CENTER);
-		
-		Button btnInitiateProcessing = new Button(container, SWT.NONE);
+
+		btnInitiateProcessing = new Button(container, SWT.NONE);
 		btnInitiateProcessing.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, false, false, 1, 1));
 		btnInitiateProcessing.setText("Initiate Processing");
 		btnInitiateProcessing.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseDown(MouseEvent e) {
-				initiateRecommendation();
+				if(isCanExecuteACC()){
+					setErrorMessage(null);
+					initiateAcc();
+				}else{
+					setErrorMessage("ACC already executed. Please, go to the next page.");
+				}
 			}
 		});
-		
+
 		Label label_1 = new Label(container, SWT.NONE);
 		label_1.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, false, false, 1, 1));
 		label_1.setText("Please, wait while is performed the process of conformance check.");
 		label_1.setAlignment(SWT.CENTER);
 	}
 
-	private void initiateRecommendation() {
+	private void initiateAcc() {
 		archKDMWizard = (ArchKDMWizard)this.getWizard();
-		
+
 		try {
 			// puts the data into a database ...
 			getContainer().run(true, true, new IRunnableWithProgress() {
@@ -90,31 +103,18 @@ public class Page04ArchitecturalCompilanceChecking extends WizardPage {
 
 					monitor.beginTask("Architectural Compilance Checking", 100);
 					monitor.worked(0);
-					
-					ArchitecturalCompilanceChecking acc = 
-							new ArchitecturalCompilanceChecking(
-									archKDMWizard.getSegmentPlannedArchitecture(), 
-									archKDMWizard.getSegmentActualArchitectureCompleteMap(), 
-									archKDMWizard.getCodeActualArchitecture(),
-									monitor);
-					
-					StructureModel violations = acc.executeAcc();
-					
-					if(violations != null){
-						setErrorMessage(null);
-						archKDMWizard.setStructureDrifts(violations);
-						save();
-						
-						setCanFlip(true);
-						setAnalisisComplete(true);
+
+					if("page02".equalsIgnoreCase(previousPage)){
+						accFromPage02(monitor);
+					}else if("page03".equalsIgnoreCase(previousPage)){
+						accFromPage03(monitor);
 					}else{
-						setErrorMessage("Fail to execute de compilance checking.");
-						System.out.println("fail to generate the structure violations");
+						failInWizard();
 					}
 					monitor.done();
 				}
 			});
-			
+
 			getWizard().getContainer().updateButtons();
 		} catch (InvocationTargetException ex) {
 			ex.printStackTrace();
@@ -122,7 +122,7 @@ public class Page04ArchitecturalCompilanceChecking extends WizardPage {
 			ex.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * @author Landi
 	 */
@@ -133,13 +133,14 @@ public class Page04ArchitecturalCompilanceChecking extends WizardPage {
 
 	@Override
 	public IWizardPage getNextPage() {
-		if(isAnalisisComplete()){
+		if(isACCComplete()){
 			return getWizard().getPage("page05");
 		}else{
+			((Page05ViewDriftsFail) getWizard().getPage("page05_1")).setLog(accLog);
 			return getWizard().getPage("page05_1");
 		}
 	}
-	
+
 	@Override
 	public boolean canFlipToNextPage() {
 		return isCanFlip();
@@ -153,12 +154,190 @@ public class Page04ArchitecturalCompilanceChecking extends WizardPage {
 		this.canFlip = canFlip;
 	}
 
-	public boolean isAnalisisComplete() {
-		return analisisComplete;
+	public boolean isACCComplete() {
+		return accComplete;
 	}
 
-	public void setAnalisisComplete(boolean analisisComplete) {
-		this.analisisComplete = analisisComplete;
+	public void setACCComplete(boolean analisisComplete) {
+		this.accComplete = analisisComplete;
+	}
+
+	/**
+	 * @author Landi
+	 * @param string
+	 */
+	public void setPreviousPage(String previousPage) {
+		this.previousPage  = previousPage;
+		enableButtons();
+	}
+
+	/**
+	 * @author Landi
+	 * @param monitor
+	 */
+	private void accFromPage02(IProgressMonitor monitor) {
+		nameActualStructure = "";
+		namePlannedStructure = "";
+
+		Display.getDefault().syncExec(new Runnable() {
+			public void run() {
+
+				String textInterface = "Select the planned architectural model:";
+				Map<String, java.util.List<StructureModel>> allStructure = GenericMethods.getAllStructure(((ArchKDMWizard)getWizard()).getSegmentPlannedArchitecture());
+				StructureModel plannedArchitecture = null;
+				if(allStructure.keySet().size() == 1){
+					for (String key : allStructure.keySet()) {
+						if(allStructure.get(key).size() == 1){
+							plannedArchitecture = allStructure.get(key).get(0); 
+						}else{
+							plannedArchitecture = (StructureModel) InterfaceGenericMethods.dialogWhatModelUse(allStructure.get(key), textInterface, getShell());
+						}
+					}
+				}else{
+					plannedArchitecture = (StructureModel) InterfaceGenericMethods.dialogWhatModelUse(allStructure, textInterface, getShell());
+				}
+
+
+
+				String textInterface2 = "Select the actual architectural model (Structure Model):";
+				Map<String, java.util.List<StructureModel>> allStructure1 = GenericMethods.getAllStructure(((ArchKDMWizard)getWizard()).getSegmentActualArchitectureCompleteMap());
+				StructureModel actualArchitecture = null;
+				if(allStructure1.keySet().size() == 1){
+					for (String key : allStructure1.keySet()) {
+						if(allStructure1.get(key).size() == 1){
+							actualArchitecture = allStructure1.get(key).get(0); 
+						}else{
+							actualArchitecture = (StructureModel) InterfaceGenericMethods.dialogWhatModelUse(allStructure1.get(key), textInterface2, getShell());
+						}
+					}
+				}else{
+					actualArchitecture = (StructureModel) InterfaceGenericMethods.dialogWhatModelUse(allStructure1, textInterface2, getShell());
+				}
+
+				String textInterface3 = "Select the actual architecture model (Code Model):";
+				Map<String, java.util.List<CodeModel>> allCode = GenericMethods.getAllCode(((ArchKDMWizard)getWizard()).getSegmentActualArchitectureCompleteMap());
+				((ArchKDMWizard)getWizard()).setCodeActualArchitecture(null);
+				if(allCode.keySet().size() == 1){
+					for (String key : allCode.keySet()) {
+						if(allCode.get(key).size() == 1){
+							((ArchKDMWizard)getWizard()).setCodeActualArchitecture(allCode.get(key).get(0)); 
+						}else{
+							((ArchKDMWizard)getWizard()).setCodeActualArchitecture((CodeModel) InterfaceGenericMethods.dialogWhatModelUse(allCode.get(key), textInterface3, getShell()));
+						}
+					}
+				}else{
+					((ArchKDMWizard)getWizard()).setCodeActualArchitecture((CodeModel) InterfaceGenericMethods.dialogWhatModelUse(allCode, textInterface3, getShell()));
+				}
+
+				if(plannedArchitecture != null && actualArchitecture != null){
+					namePlannedStructure = plannedArchitecture.getName();
+					nameActualStructure = actualArchitecture.getName();
+				}else{
+					setErrorMessage("Select the planned and the actual architecture before continue.");
+				}
+
+			}
+		});
+
+		if(!"".equalsIgnoreCase(namePlannedStructure) && !"".equalsIgnoreCase(nameActualStructure)){
+			if(((ArchKDMWizard)getWizard()).getCodeActualArchitecture() != null){
+				executeACC(monitor, namePlannedStructure, nameActualStructure);
+			}else{
+				Display.getDefault().syncExec(new Runnable() {
+					public void run() {	
+						setErrorMessage("Select the actual code model before continue.");
+					}
+				});
+			}
+		}
+	}
+
+	/**
+	 * 
+	 * @author Landi
+	 * @param monitor
+	 */
+	private void accFromPage03(IProgressMonitor monitor) {
+		String namePlannedStructure = "PlannedArchitecture";
+		String nameActualStructure = "CompleteMap";
+		executeACC(monitor, namePlannedStructure, nameActualStructure);
+	}
+
+	/**
+	 * 
+	 * @author Landi
+	 * @param monitor
+	 * @param namePlannedStructure
+	 * @param nameActualStructure
+	 */
+	private void executeACC(IProgressMonitor monitor, String namePlannedStructure, String nameActualStructure) {
+		ArchitecturalCompilanceChecking acc = 
+				new ArchitecturalCompilanceChecking(
+						archKDMWizard.getSegmentPlannedArchitecture(), 
+						archKDMWizard.getSegmentActualArchitectureCompleteMap(), 
+						archKDMWizard.getCodeActualArchitecture(),
+						monitor,
+						namePlannedStructure,
+						nameActualStructure);
+
+		StructureModel violations = acc.executeAcc();
+
+		if(violations != null){
+			archKDMWizard.setStructureDrifts(violations);
+			save();
+
+			setCanFlip(true);
+			setACCComplete(true);
+			Display.getDefault().syncExec(new Runnable() {
+				public void run() {
+					setErrorMessage(null);
+					disableButtons();
+				}
+			});
+
+		}else{
+			ArchitecturalCompilanceChecking.appendMessageToLog("Fail to execute de compilance checking.");
+			setACCComplete(false);
+			accLog = acc.getMessageLog();
+		}
+	}
+
+	/**
+	 * @author Landi
+	 */
+	protected void disableButtons() {
+		setCanExecuteAcc(false);
+	}
+	/**
+	 * @author Landi
+	 * @param b
+	 */
+	private void setCanExecuteAcc(boolean canExecuteACC) {
+		this.canExecuteACC = canExecuteACC;
 	}
 	
+	/**
+	 * @return the canExecuteACC
+	 */
+	public boolean isCanExecuteACC() {
+		return canExecuteACC;
+	}
+
+	/**
+	 * @author Landi
+	 */
+	protected void enableButtons() {
+		setCanExecuteAcc(true);
+	}
+
+	/**
+	 * @author Landi
+	 */
+	private void failInWizard() {
+		accLog.append("\n");
+		accLog.append("Falha ao executar o plug-in");
+		accLog.append("\n");
+		setACCComplete(false);
+	}
+
 }
